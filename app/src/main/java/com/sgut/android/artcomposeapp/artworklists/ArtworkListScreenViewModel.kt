3e84b57fd2a073fff46dc1198cab4a1e5f7d1542
salgut408 <sgutie@saic.edu>
2022-09-22 14:48:52 -0500

    added Repository

--- app/src/main/java/com/sgut/android/artcomposeapp/artworklists/ArtworkListScreenViewModel.kt
+++ app/src/main/java/com/sgut/android/artcomposeapp/artworklists/ArtworkListScreenViewModel.kt
@@ -40,6 +40,7 @@ class ArtworkListScreenViewModel @Inject constructor(
             when (result) {
                 is Resource.Success -> {
                     //TODO move to repository
+
                     val artEntries = result.data?.artworkList?.map { entry ->
                         ArtworkModel(
                             entry.id,
